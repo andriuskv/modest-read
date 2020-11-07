c462bf759f9bc6c7f164f208fc96b516db4ce972
Andrius Kvedaras <ikandrius@gmail.com>
2020-11-07 15:48:08 +0200

    Add PDF link support

---
+++ src/services/viewerLinkService.js
@@ -0,0 +1,48 @@
+import { scrollToPage } from "../utils";
+import { getSetting } from "./settingsService";
+
+export default class LinkService {
+  constructor(pdfDocument, pdfElement, url) {
+    this.pdfDocument = pdfDocument;
+    this.pdfElement = pdfElement;
+    this.url = url;
+  }
+
+  navigateTo(dest) {
+    this.goToDestination(dest);
+  }
+
+  async goToDestination(dest) {
+    let explicitDest = null;
+
+    if (typeof dest === "string") {
+      explicitDest = await this.pdfDocument.getDestination(dest);
+    }
+    else {
+      explicitDest = dest;
+    }
+    const index = await this.pdfDocument.getPageIndex(explicitDest[0]);
+
+    scrollToPage(index + 1, this.pdfElement.children, getSetting("keepToolbarVisible"));
+  }
+
+  getDestinationHash(dest) {
+    if (typeof dest === "string") {
+      if (dest.length) {
+        return this.getAnchorUrl(`#${escape(dest)}`);
+      }
+    }
+    if (Array.isArray(dest)) {
+      const str = JSON.stringify(dest);
+
+      if (str.length) {
+        return this.getAnchorUrl(`#${escape(str)}`);
+      }
+    }
+    return this.getAnchorUrl("");
+  }
+
+  getAnchorUrl(anchor) {
+    return this.url + anchor;
+  }
+}
