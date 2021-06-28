a62b3b3d19683803efcd2c10089afcc96e0683b8
Andrius Kvedaras <ikandrius@gmail.com>
2021-06-28 16:00:35 +0300

    Change statistics format

--- src/services/readingTimeService.js
+++ src/services/readingTimeService.js
@@ -4,6 +4,21 @@ const readingTime = JSON.parse(localStorage.getItem("reading-time")) || {};
 let timeStamp = 0;
 let timeStampIntervalId = 0;
 
+(function() {
+  let changed = false;
+
+  for (const year of Object.keys(readingTime)) {
+    if (Array.isArray(readingTime[year])) {
+      changed = true;
+      delete readingTime[year];
+    }
+  }
+
+  if (changed) {
+    localStorage.setItem("reading-time", JSON.stringify(readingTime));
+  }
+})();
+
 function startCounting() {
   timeStamp = Date.now();
   timeStampIntervalId = setInterval(saveTimeStamp, 10000);
@@ -31,9 +46,11 @@ function saveTimeStamp() {
   const { year, month, day } = getCurrentDate();
   const diff = Date.now() - timeStamp;
 
-  readingTime[year] = readingTime[year] || getCalendarYear(year);
-  readingTime[year][month][day] += Math.round(diff / 1000);
+  readingTime[year] = readingTime[year] || {};
+  readingTime[year][month] = readingTime[year][month] || {};
+  readingTime[year][month][day] = (readingTime[year][month][day] || 0) + Math.round(diff / 1000);
   timeStamp = Date.now();
+
   localStorage.setItem("reading-time", JSON.stringify(readingTime));
 }
 
